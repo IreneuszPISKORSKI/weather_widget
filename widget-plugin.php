4bdd118fb47636c453d4b43c60d4e1bfbdc8c08f
Irenusz <ireneusz.piskorski@le-campus-numerique.fr>
2023-08-10 14:48:14 +0200

    first commit

---
+++ widget-plugin.php
@@ -0,0 +1,143 @@
+<?php
+/*
+Plugin Name: Weather Widget Plugin
+Plugin URI:
+Description: This plugin adds a weather widget.
+Version: 1.0
+Author: Irek
+Author URI:
+License: GPL2
+*/
+
+// The widget class
+class My_Custom_Widget extends WP_Widget {
+
+    // Main constructor
+    public function __construct() {
+        parent::__construct(
+            'my_weather_widget',
+            __( 'My Weather Widget', 'text_domain' ),
+            array(
+                'customize_selective_refresh' => true,
+            )
+        );
+    }
+
+    // The widget form (for the backend )
+    public function form( $instance ) {
+
+        // Set widget defaults
+        $defaults = array(
+            'title'    => 'Test',
+            'select_city'   => '',
+            'select_country'   => '',
+        );
+
+        // Parse current settings with defaults
+        extract( wp_parse_args( ( array ) $instance, $defaults ) ); ?>
+
+        <?php // Widget Title ?>
+
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Widget Title', 'text_domain' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
+
+        <?php // Dropdown ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'select_city' ); ?>"><?php _e( 'Select city', 'text_domain' ); ?></label>
+            <select name="<?php echo $this->get_field_name( 'select_city' ); ?>" id="<?php echo $this->get_field_id( 'select_city' ); ?>" class="widefat">
+                <?php
+                // Your options array
+                $options = array(
+                    ''        => __( 'Select City', 'text_domain' ),
+                    'Grenoble' => __( 'Grenoble', 'text_domain' ),
+                    'Crest' => __( 'Crest', 'text_domain' ),
+                );
+
+                // Loop through options and add each one to the select dropdown
+                foreach ( $options as $key => $name ) {
+                    echo '<option value="' . esc_attr( $key ) . '" id="' . esc_attr( $key ) . '" '. selected( $select_city, $key, false ) . '>'. $name . '</option>';
+
+                } ?>
+            </select>
+        </p>
+
+        <?php // Dropdown 2 ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'select_country' ); ?>"><?php _e( 'Select country', 'text_domain' ); ?></label>
+            <select name="<?php echo $this->get_field_name( 'select_country' ); ?>" id="<?php echo $this->get_field_id( 'select_country' ); ?>" class="widefat">
+                <?php
+                // Your options array
+                $options = array(
+                    ''        => __( 'Select Country', 'text_domain' ),
+                    'France' => __( 'France', 'text_domain' ),
+                );
+
+                // Loop through options and add each one to the select dropdown
+                foreach ( $options as $key => $name ) {
+                    echo '<option value="' . esc_attr( $key ) . '" id="' . esc_attr( $key ) . '" '. selected( $select_country, $key, false ) . '>'. $name . '</option>';
+
+                } ?>
+            </select>
+        </p>
+
+    <?php }
+
+    // Update widget settings
+    public function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance['title']    = isset( $new_instance['title'] ) ? wp_strip_all_tags( $new_instance['title'] ) : '';
+        $instance['select_city']   = isset( $new_instance['select_city'] ) ? wp_strip_all_tags( $new_instance['select_city'] ) : '';
+        $instance['select_country']   = isset( $new_instance['select_country'] ) ? wp_strip_all_tags( $new_instance['select_country'] ) : '';
+        return $instance;
+    }
+
+    // Display the widget
+    public function widget( $args, $instance ) {
+
+        extract( $args );
+
+        // Check the widget options
+        $title    = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
+        $select_city   = isset( $instance['select_city'] ) ? $instance['select_city'] : '';
+        $select_country   = isset( $instance['select_country'] ) ? $instance['select_country'] : '';
+        $language = 'french';
+
+        $api_url = "https://www.weatherwp.com/api/common/publicWeatherForLocation.php?city=$select_city&country=$select_country&language=$language";
+        $response = wp_remote_get($api_url);
+
+        $data = json_decode(wp_remote_retrieve_body($response));
+
+        $temperature = $data->temp;
+        $icon_url = $data->icon;
+        $description = $data->description;
+
+        // WordPress core before_widget hook (always include )
+        echo $before_widget;
+
+        // Display the widget
+        ?>
+        <div class="cnalps-weather-widget">
+            <div class="weather-title">Météo à <? echo $select_city . ' - ' . $select_country ?></div>
+        </div>
+        <div>
+            <?
+            echo "$temperature °C - $description";
+            echo "<img src='$icon_url' alt='Weather Icon'>";
+            ?>
+        </div>
+        <?php
+
+        // WordPress core after_widget hook (always include )
+        echo $after_widget;
+
+    }
+
+}
+
+// Register the widget
+function my_register_custom_widget() {
+    register_widget( 'My_Weather_Widget' );
+}
+add_action( 'widgets_init', 'my_register_weather_widget' );
